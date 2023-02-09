a3db2fcc74f755fd265a7d680b1c762292717de7
Ivson Tavares <ivsont87@gmail.com>
2023-02-08 22:13:15 -0300

    Tela Login Pronta

---
+++ ManGed/Controllers/LoginController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ManGed.Controllers
+{
+    public class LoginController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
