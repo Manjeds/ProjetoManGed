e038c893713612048c26f5a2ca4d6f01f8c738ee
Danilo Victor <danilo-almeida12@hotmail.com>
2023-02-09 22:00:46 -0300

    Controller ResetPass

---
+++ ManGed/Controllers/ResetPassController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ManGed.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult ResetPass()
+        {
+            return View();
+        }
+    }
+}
